3dd42733574ee9396052747171c1e7fe068fb95f
nathan moreau <nathan.moreau@m4x.org>
2017-06-12 13:36:49 +0200

    Fix escaping in here document.

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ editor = emacs -Q --eval '(progn (push (cons (quote font) \"Consolas 16\") defau
     laa = log --all --decorate --oneline --graph --color
     ll = log --decorate --oneline --color
     r1 = reset HEAD^
-    rw = "!f() { git checkout -b review_$1 origin/$1; }; f"
+    rw = "!f() { git checkout -b review_\$1 origin/\$1; }; f"
     rh= reset --hard
     sn = show --name-only
     s = status
@@ -40,7 +40,7 @@ editor = emacs -Q --eval '(progn (push (cons (quote font) \"Consolas 16\") defau
 [merge]
     tool = ediff
 [mergetool "ediff"]
-    cmd = emacs --eval \"(ediff-merge-files-with-ancestor \\\"$LOCAL\\\" \\\"$REMOTE\\\" \\\"$BASE\\\" nil \\\"$MERGED\\\")\"
+    cmd = emacs --eval \"(ediff-merge-files-with-ancestor \\\"\$LOCAL\\\" \\\"\$REMOTE\\\" \\\"\$BASE\\\" nil \\\"\$MERGED\\\")\"
 [color "diff"]
     frag = magenta bold
     meta = yellow bold
