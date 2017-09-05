881f0652bb4780484691e10e681e8dcc7fa404df
nathan moreau <nathan.moreau@m4x.org>
2017-09-05 15:56:30 +0200

    Stop setting PROMPT_COMMAND.

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ DOTFILEDIR=$(dirname "$0")
 
 cat<<EOF
 PS1='\[\033[01;33m\]\w\[\033[00m\]$ '
-PROMPT_COMMAND='echo -ne "\033]0;\${PWD}\007"'
 alias s='git status || ls'
 alias d='git diff'
 alias dc='git diff --cached'
