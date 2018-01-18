06c1a657d5971afa2dd1f20b29aa8a1a05ccd41c
nathan moreau <nathan.moreau@m4x.org>
2018-01-18 11:05:31 +0100

    alias: fix.

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ esac
 
 PS1='\[\033[01;33m\]\w\[\033[01;32m\][$?]\[\033[01m\]\[\033[01;37m\]$\[\033[00m\] '
 alias cdd='pushd $DOTFILEDIR'
-alias cde='pushd $EMACSD'
+alias cde='pushd $HOME/.emacs.d'
 alias cdp='pushd $PROJECTS'
 alias d='git diff'
 alias dc='git diff --cached'
