dabb7e05520891a37988a24d4428dd56da437f98
nathan moreau <nathan.moreau@m4x.org>
2018-01-24 02:58:14 +0100

    Colors in manpages.

--- setup.sh
+++ setup.sh
@@ -59,6 +59,18 @@ mkcdir () {
         fi
 }
 
+man() {
+        env \
+                LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
+                LESS_TERMCAP_md="$(printf "\e[1;31m")" \
+                LESS_TERMCAP_me="$(printf "\e[0m")" \
+                LESS_TERMCAP_se="$(printf "\e[0m")" \
+                LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
+                LESS_TERMCAP_ue="$(printf "\e[0m")" \
+                LESS_TERMCAP_us="$(printf "\e[1;32m")" \
+                man "${@}"
+}
+
 if command -v openbox >/dev/null
 then
         OPENBOX_CONFIG="$HOME/.config/openbox"
