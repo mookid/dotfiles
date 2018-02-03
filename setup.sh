6c9740e9263a81c2ae4b7b1d3f8001b4d0a6d3a0
nathan moreau <nathan.moreau@m4x.org>
2018-02-03 01:12:46 +0100

    Add command to clone git repos to specific directory.

--- setup.sh
+++ setup.sh
@@ -35,6 +35,7 @@ EOF
 esac
 
 PS1='\[\033[01;33m\]\w\[\033[01;32m\][$?]\[\033[01m\]\[\033[01;37m\]$\[\033[00m\] '
+export GITHUBPROJECTS="$HOME/projects"
 CDPATH="$HOME"
 alias cdd='pushd $DOTFILEDIR'
 alias cde='pushd $HOME/.emacs.d'
