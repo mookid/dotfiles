eef2c7790ad0548d570af242a33e93488b8e4a15
Nathan Moreau <nathan.moreau@m4x.org>
2020-02-06 21:56:48 +0100

    windows-terminal: sync: robustify.

--- windows-terminal-config/sync.bat
+++ windows-terminal-config/sync.bat
@@ -1,3 +1,3 @@
 @echo off
 
-copy profiles.json "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+copy %~dp0\profiles.json "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
