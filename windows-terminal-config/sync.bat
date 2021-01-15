c4d40d89f886f95328bfce06c0c564d22d2aa32d
Nathan Moreau <nathan.moreau@m4x.org>
2021-01-15 02:20:59 +0100

    windows terminal: update to new file name.

--- windows-terminal-config/sync.bat
+++ windows-terminal-config/sync.bat
@@ -1,3 +1,7 @@
 @echo off
 
-copy %~dp0\profiles.json "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+echo copying profiles.json...
+copy %~dp0\profiles.json "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+echo copying bg01.jpg...
+copy %~dp0\bg01.jpg "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
