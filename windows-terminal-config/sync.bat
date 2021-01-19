6638ea1e5ea13c6eb89ba3739f7262925736f62e
Nathan Moreau <nathan.moreau@m4x.org>
2021-01-19 01:28:22 +0100

    windows terminal: change indentation level.

--- windows-terminal-config/sync.bat
+++ windows-terminal-config/sync.bat
@@ -1,7 +1,9 @@
 @echo off
 
+set wtDir=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+
 echo copying profiles.json...
-copy %~dp0\profiles.json "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+copy %~dp0\profiles.json "%wtDir%\settings.json"
 
 echo copying bg01.jpg...
-copy %~dp0\bg01.jpg "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+copy %~dp0\bg01.jpg %wtDir%
